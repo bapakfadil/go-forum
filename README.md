712f8be1a9c40f840486ac3e36318a002ae4ae71
bapakfadil <sipadil28@hotmail.com>
2025-06-07 17:55:54 +0700

    Initiate project: Adding Go app configuration and database migrations

---
+++ README.md
@@ -0,0 +1,21 @@
+# Go Discussion Forum (Under Development)
+
+Welcome to the repository for the discussion forum project I'm currently building using the Go programming language! This project aims to create an interactive platform where users can discuss, share ideas, and interact within a community environment.
+
+## About This Project
+
+Ongoing project.
+
+## Technologies Used
+
+Technlogies used so far :
+
+- **Go (Golang):** The primary programming language for the backend.
+
+## Project Status
+
+This project is **currently in active development**.
+
+## License
+
+This project is licensed under the [License Name, e.g., MIT License]. See the `LICENSE
---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/bapakfadil/fastcampus/internal/configs"
+	"github.com/bapakfadil/fastcampus/internal/handlers/memberships"
+	membershipRepo "github.com/bapakfadil/fastcampus/internal/repositories/memberships"
+	"github.com/bapakfadil/fastcampus/pkg/internalsql"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	var (
+		cfg *configs.Config
+	)
+
+	err := configs.Init(
+		configs.WithConfigFolder(
+			[]string{"./internal/configs"},
+		),
+		configs.WithConfigFile("config"),
+		configs.WithConfigType("yaml"),
+	)
+
+	if err != nil {
+		log.Fatal("Gagal inisiasi config!")
+	}
+
+	cfg = configs.Get()
+	log.Println("config", cfg)
+
+	db, err := internalsql.Connect(cfg.Database.DataSourceName)
+	if err != nil {
+		log.Fatal("Gagal inisiasi database!", err)
+	}
+
+	_ = membershipRepo.NewRepository(db)
+	
+	membershipHandler := memberships.NewHandler(r)
+	membershipHandler.RegisterRoute()
+
+	r.Run(cfg.Service.Port) // listen and run the server
+}
